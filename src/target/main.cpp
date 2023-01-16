e2160087099b4f2086b36471ac16555009cbbb4d
Redstonerayy <antonpaulr@protonmail.com>
2023-01-16 19:59:48 +0100

    prototyping accompanying sheet

--- src/target/main.cpp
+++ src/target/main.cpp
@@ -7,7 +7,7 @@ int main(){
     std::vector<double> inputs = {1.0f, 1.0f};
     neuralnetwork->addlayer(SIGMOID_FUNCTION, inputs.size());
     neuralnetwork->addlayer(HTAN_FUNCTION, 3);
-    neuralnetwork->addlayer(SIGMOID_FUNCTION, 1);
+    neuralnetwork->addlayer(SIGMOID_FUNCTION, 2);
     neuralnetwork->connectalllayers();
     neuralnetwork->evaluatenetwork(inputs);
 }
